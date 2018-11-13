03534aa47cf437a18461a456f5145d7a666cd698
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-11-13 10:55:00 +0900

    Add SHA1 spec

---
+++ spec/webpage_update_checker/methods/sha1_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe WebpageUpdateChecker::Methods::Sha1 do
+  describe '#current_diff_value' do
+    let(:sha1) { WebpageUpdateChecker::Methods::Sha1.new(uri: 'https://www.yahoo.co.jp/') }
+
+    it 'returns SHA1 hash format' do
+      expect(sha1.current_diff_value).to match(/\A[a-f0-9]{40}\z/)
+    end
+  end
+end
