1218dcb8f0d9809f6bf0297dcba0365eb04e26eb
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-11-25 17:30:47 +0900

    Update spec because of adding new method

--- spec/webpage_update_checker/client_spec.rb
+++ spec/webpage_update_checker/client_spec.rb
@@ -15,20 +15,68 @@ RSpec.describe WebpageUpdateChecker::Client do
     end
   end
 
+  describe '#current_diff_value' do
+    it 'returns true if uri is valid' do
+      obj = WebpageUpdateChecker::Methods::Md5.new(uri: 'https://www.yahoo.co.jp/')
+      expect(@checker.current_diff_value(obj)).to be_truthy
+    end
+
+    it 'returns Exceotion if uri is invalid' do
+      obj = WebpageUpdateChecker::Methods::Md5.new(uri: 'https://foobar/')
+      expect { @checker.current_diff_value(obj) }.to raise_error(SocketError)
+    end
+  end
+
   describe '#updated?' do
-    context 'when the Methods are hash algorithm' do
-      it 'return true if hash is different from previous one' do
-        # TODO: Assumed this page's MD5 is not changed
-        current_hash_value  = WebpageUpdateChecker::Methods::Md5.new(uri: 'http://www.ne.jp/asahi/pursuits/ootsuki/pc/X68k/x680x0.xhtml')
-        previous_hash_value = 'fb2eeb524e6b5637ef7fa4bed48f1d68'
-        expect(@checker.updated?(current_hash_value, previous_comparison_value: previous_hash_value)).to eq true
+    context 'when "previous_comparison_value" keyword argument is set' do
+      context 'when the Methods are hash algorithm' do
+        it 'return true if hash is different from previous one' do
+          # TODO: Assumed this page's MD5 is not changed
+          current_hash_value  = WebpageUpdateChecker::Methods::Md5.new(uri: 'http://www.ne.jp/asahi/pursuits/ootsuki/pc/X68k/x680x0.xhtml')
+          previous_hash_value = 'fb2eeb524e6b5637ef7fa4bed48f1d68'
+          expect(@checker.updated?(current_hash_value, previous_comparison_value: previous_hash_value)).to eq true
+        end
+
+        it 'return false if hash is different from previous one' do
+          # TODO: Assumed this page's MD5 is not changed
+          current_hash_value  = WebpageUpdateChecker::Methods::Md5.new(uri: 'http://www.ne.jp/asahi/pursuits/ootsuki/pc/X68k/x680x0.xhtml')
+          previous_hash_value = 'fb2eeb524e6b5637ef7fa4bed48f1d67'
+          expect(@checker.updated?(current_hash_value, previous_comparison_value: previous_hash_value)).to eq false
+        end
       end
 
-      it 'return false if hash is different from previous one' do
-        # TODO: Assumed this page's MD5 is not changed
-        current_hash_value  = WebpageUpdateChecker::Methods::Md5.new(uri: 'http://www.ne.jp/asahi/pursuits/ootsuki/pc/X68k/x680x0.xhtml')
-        previous_hash_value = 'fb2eeb524e6b5637ef7fa4bed48f1d67'
-        expect(@checker.updated?(current_hash_value, previous_comparison_value: previous_hash_value)).to eq false
+      context 'when the return type of the Methods is Time class' do
+        # pending (skip)
+      end
+
+      context 'when the return type of the Methods is Integer class' do
+        # pending (skip)
+      end
+    end
+
+    context 'when "previous_diff_value" keyword argument is set' do
+      context 'when the Methods are hash algorithm' do
+        it 'return true if hash is different from previous one' do
+          # TODO: Assumed this page's MD5 is not changed
+          current_hash_value  = WebpageUpdateChecker::Methods::Md5.new(uri: 'http://www.ne.jp/asahi/pursuits/ootsuki/pc/X68k/x680x0.xhtml')
+          previous_hash_value = 'fb2eeb524e6b5637ef7fa4bed48f1d68'
+          expect(@checker.updated?(current_hash_value, previous_diff_value: previous_hash_value)).to eq true
+        end
+
+        it 'return false if hash is different from previous one' do
+          # TODO: Assumed this page's MD5 is not changed
+          current_hash_value  = WebpageUpdateChecker::Methods::Md5.new(uri: 'http://www.ne.jp/asahi/pursuits/ootsuki/pc/X68k/x680x0.xhtml')
+          previous_hash_value = 'fb2eeb524e6b5637ef7fa4bed48f1d67'
+          expect(@checker.updated?(current_hash_value, previous_diff_value: previous_hash_value)).to eq false
+        end
+      end
+
+      context 'when the return type of the Methods is Time class' do
+        # pending (skip)
+      end
+
+      context 'when the return type of the Methods is Integer class' do
+        # pending (skip)
       end
     end
   end
