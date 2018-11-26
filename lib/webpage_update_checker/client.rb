410a5f44748210d76d145d8025ec48640391665c
Osamu Takiya <takiya@toran.sakura.ne.jp>
2018-11-26 20:20:30 +0900

    Add lazy_time_updated method

--- lib/webpage_update_checker/client.rb
+++ lib/webpage_update_checker/client.rb
@@ -1,4 +1,6 @@
 require 'mechanize'
+require 'active_support'
+require 'active_support/core_ext'
 Dir[File.expand_path('methods', __dir__) << '/*.rb'].each { |file| require file }
 
 module WebpageUpdateChecker
@@ -10,6 +12,13 @@ module WebpageUpdateChecker
       else
         current_comparison_value(checked_object) != previous_comparison_value
       end
+    end
+
+    def lazy_time_updated?(checked_object, previous_comparison_value:, lazy_seconds:)
+      corrected_previous_comparison_value_time_class = (Time.parse(previous_comparison_value) + lazy_seconds.seconds).utc
+      checked_object_time_class = current_comparison_value(checked_object).utc
+
+      checked_object_time_class > corrected_previous_comparison_value_time_class
     end
 
     def current_comparison_value(checked_object)
--- spec/webpage_update_checker/client_spec.rb
+++ spec/webpage_update_checker/client_spec.rb
@@ -32,16 +32,16 @@ RSpec.describe WebpageUpdateChecker::Client do
       context 'when the Methods are hash algorithm' do
         it 'return true if hash is different from previous one' do
           # TODO: Assumed this page's MD5 is not changed
-          current_hash_value  = WebpageUpdateChecker::Methods::Md5.new(uri: 'http://www.ne.jp/asahi/pursuits/ootsuki/pc/X68k/x680x0.xhtml')
+          current_hash_object  = WebpageUpdateChecker::Methods::Md5.new(uri: 'http://www.ne.jp/asahi/pursuits/ootsuki/pc/X68k/x680x0.xhtml')
           previous_hash_value = 'fb2eeb524e6b5637ef7fa4bed48f1d68'
-          expect(@checker.updated?(current_hash_value, previous_comparison_value: previous_hash_value)).to eq true
+          expect(@checker.updated?(current_hash_object, previous_comparison_value: previous_hash_value)).to eq true
         end
 
         it 'return false if hash is different from previous one' do
           # TODO: Assumed this page's MD5 is not changed
-          current_hash_value  = WebpageUpdateChecker::Methods::Md5.new(uri: 'http://www.ne.jp/asahi/pursuits/ootsuki/pc/X68k/x680x0.xhtml')
+          current_hash_object  = WebpageUpdateChecker::Methods::Md5.new(uri: 'http://www.ne.jp/asahi/pursuits/ootsuki/pc/X68k/x680x0.xhtml')
           previous_hash_value = 'fb2eeb524e6b5637ef7fa4bed48f1d67'
-          expect(@checker.updated?(current_hash_value, previous_comparison_value: previous_hash_value)).to eq false
+          expect(@checker.updated?(current_hash_object, previous_comparison_value: previous_hash_value)).to eq false
         end
       end
 
@@ -58,16 +58,16 @@ RSpec.describe WebpageUpdateChecker::Client do
       context 'when the Methods are hash algorithm' do
         it 'return true if hash is different from previous one' do
           # TODO: Assumed this page's MD5 is not changed
-          current_hash_value  = WebpageUpdateChecker::Methods::Md5.new(uri: 'http://www.ne.jp/asahi/pursuits/ootsuki/pc/X68k/x680x0.xhtml')
+          current_hash_object  = WebpageUpdateChecker::Methods::Md5.new(uri: 'http://www.ne.jp/asahi/pursuits/ootsuki/pc/X68k/x680x0.xhtml')
           previous_hash_value = 'fb2eeb524e6b5637ef7fa4bed48f1d68'
-          expect(@checker.updated?(current_hash_value, previous_diff_value: previous_hash_value)).to eq true
+          expect(@checker.updated?(current_hash_object, previous_diff_value: previous_hash_value)).to eq true
         end
 
         it 'return false if hash is different from previous one' do
           # TODO: Assumed this page's MD5 is not changed
-          current_hash_value  = WebpageUpdateChecker::Methods::Md5.new(uri: 'http://www.ne.jp/asahi/pursuits/ootsuki/pc/X68k/x680x0.xhtml')
+          current_hash_object  = WebpageUpdateChecker::Methods::Md5.new(uri: 'http://www.ne.jp/asahi/pursuits/ootsuki/pc/X68k/x680x0.xhtml')
           previous_hash_value = 'fb2eeb524e6b5637ef7fa4bed48f1d67'
-          expect(@checker.updated?(current_hash_value, previous_diff_value: previous_hash_value)).to eq false
+          expect(@checker.updated?(current_hash_object, previous_diff_value: previous_hash_value)).to eq false
         end
       end
 
@@ -77,6 +77,30 @@ RSpec.describe WebpageUpdateChecker::Client do
 
       context 'when the return type of the Methods is Integer class' do
         # pending (skip)
+      end
+    end
+  end
+
+  describe '#lazy_time_updated?' do
+    context 'when comparision values with lazy_seconds are the same' do
+      it 'return false' do
+        # 2008-03-18 01:05:01 +0900
+        current_time_object = WebpageUpdateChecker::Methods::LastModified.new(uri: 'http://www.ne.jp/asahi/pursuits/ootsuki/pc/X68k/x680x0.xhtml')
+        previous_comparision_value = '2008-03-18 01:00:01 +0900'
+        lazy_seconds = 60 * 5
+
+        expect(@checker.lazy_time_updated?(current_time_object, previous_comparison_value: previous_comparision_value, lazy_seconds: lazy_seconds)).to eq false
+      end
+    end
+
+    context 'when comparision values with lazy_seconds are different' do
+      it 'return true' do
+        # 2008-03-18 01:05:01 +0900
+        current_time_object = WebpageUpdateChecker::Methods::LastModified.new(uri: 'http://www.ne.jp/asahi/pursuits/ootsuki/pc/X68k/x680x0.xhtml')
+        previous_comparision_value = '2008-03-18 01:00:00 +0900'
+        lazy_seconds = 60 * 5
+
+        expect(@checker.lazy_time_updated?(current_time_object, previous_comparison_value: previous_comparision_value, lazy_seconds: lazy_seconds)).to eq true
       end
     end
   end
